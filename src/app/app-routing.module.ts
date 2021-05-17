54358132d40e5a5a94713de9ee859a66e5882113
Sumit Rajegore <sumitrajegorevp@gmail.com>
2021-05-17 23:46:15 +0530

    Refactor : giving path for child componant

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArchiveComponent } from './componant/archive/archive.component';
 import { DashboardComponent } from './componant/dashboard/dashboard.component';
 import { DisplayComponent } from './componant/display/display.component';
+import { EditLabelsComponent } from './componant/edit-labels/edit-labels.component';
 import { ForgotpasswordComponent } from './componant/forgotpassword/forgotpassword.component';
 import { LoginComponent } from './componant/login/login.component';
+import { NotesComponent } from './componant/notes/notes.component';
 import { RagisterComponent } from './componant/ragister/ragister.component';
+import { RemindersComponent } from './componant/reminders/reminders.component';
 import { ResetpasswordComponent } from './componant/resetpassward/resetpassward.component';
+import { TrashComponent } from './componant/trash/trash.component';
 
 const routes: Routes = [
   {
@@ -30,8 +35,15 @@ const routes: Routes = [
     component: ForgotpasswordComponent
   },
   {
-    path:'dashboard',
-    component: DashboardComponent
+    path:'dashboard', component: DashboardComponent,
+      children :[
+        { path : 'notes', component: NotesComponent},
+        { path : 'reminders', component: RemindersComponent},
+        { path : 'edit-labels', component: EditLabelsComponent},
+        { path : 'archive', component: ArchiveComponent},
+        { path : 'trash', component: TrashComponent},
+        { path : "", redirectTo: "notes", pathMatch: "full" }
+      ]
   },
   {
     path:'display',
